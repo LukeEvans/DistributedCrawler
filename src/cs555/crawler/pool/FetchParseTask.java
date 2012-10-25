cf6bd6370f8d3620109afee2ef38a49a0c14339b
Luke Evans <levans002@gmail.com>
2012-10-25 16:05:27 -0600

    Added thread pool

---
+++ src/cs555/crawler/pool/FetchParseTask.java
@@ -0,0 +1,54 @@
+package cs555.crawler.pool;
+
+import org.htmlparser.beans.*;
+
+import cs555.crawler.communications.Link;
+
+public class FetchParseTask implements Task {
+
+	Link link;
+	int runningThread;
+	
+	// Fetchers
+	HTMLTextBean textFetcher;
+	HTMLLinkBean linkfetcher;
+	
+	// URL
+	String urlString;
+	
+	// Text
+	String urlText;
+	
+	
+	
+	//================================================================================
+	// Constructor
+	//================================================================================
+	public FetchParseTask(Link l, String url){
+		link = l;
+		textFetcher = new HTMLTextBean();
+		linkfetcher = new HTMLLinkBean();
+		urlString = url;
+		
+		textFetcher.setURL(url);
+		linkfetcher.setURL(url);
+	}
+	
+	//================================================================================
+	// Run
+	//================================================================================
+	public void run() {
+		//URL[] links = linkfetcher.getLinks();
+		
+	}
+
+	
+	
+	//================================================================================
+	// House Keeping
+	//================================================================================
+	public void setRunning(int i) {
+		runningThread = i;
+	}
+
+}
